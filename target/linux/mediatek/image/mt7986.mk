83d71f72e25515266e33d8248fd75c0c0a9be656
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2025-06-22 11:35:34 +0800

    Update mt7986.mk

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -537,7 +537,7 @@ TARGET_DEVICES += tplink_tl-xdr6088
 
 define Device/tplink_tl-xdr4288
   DEVICE_MODEL := TL-XDR4288
-  DEVICE_DTS := mt7986a-tl-xdr4288
+  DEVICE_DTS := mt7986a-tplink-tl-xdr4288
   $(call Device/tplink_tl-common)
 endef
 TARGET_DEVICES += tplink_tl-xdr4288
